fcc2066b8f25f91fc05ab59dd40a40825f93e0ff
Lee Hayford <leehayford@gmail.com>
2020-11-11 18:05:49 -0700

    formatted and cleaned up README.md added comments to autodjango_basic.py

--- README.md
+++ README.md
@@ -1,41 +1,57 @@
-Creates a blank-ish, one-page django project called `your_project_name` in a virtual environment and runs the server.     
-- home page: ... _localhost:8000/_
-- admin page: ... _localhost:8000/adimn/_
-- database: ... _your_project_name /src / db.sqlite3_
-- templates: ... _base.html_ ... _app_main / home.html_
+# _autodjango_basic_
 
-#### How to:
+#### What it do :
+- creates a near-blank, one-page, **django web app** in `~/your_projects_directory/your_project_name`, hence forth writen as: _~/../_
+- creates and activates a **virtual environment**
+  - installs **django 3.0.8** and dependencies
+  - starts a **django project** `project_master`
+  - starts a **django app** `app_main`
+  - creates and migrates **database :**\
+    prompts for :\
+    $ `user_name`\
+    $ `user_password`\
+    _~/../src/db.sqlite3_
+  - creates and edits **html** templates :\
+    _~/../src/templates/base.html_\
+    _~/../src/templates/app_main/home.html_
+  - and optionally, runs the server (y/n)\
+    - home page : **localhost:8000**\
+    - admin log in page : **localhost:8000/adimn**
+
+#### How to do :
 1. Clone this repo into
 _.~/your_projects_directory/_
-2. On the command line
-  - $ `cd` `your_projects_directory`
+2. _~/$_ `cd` `your_projects_directory`
+3. _~/$_ `python` `autodjango/autodjango.py` `your_project_name`
+4. Autodgango makes and edits the following directories and files:
+    -  _your_project_name / src / project_master / settings.py_
+    -  _your_project_name / src / project_master /_
+    -  _your_project_name / src / app_main /_
+    -  _your_project_name / src / app_main / migrations /_
+    -  _your_project_name / src / templates / base.html_
+    -  _your_project_name / src / templates / app_main /_
+    -  _your_project_name / src / templates / app_main / home.html_
+    -  _your_project_name / src / static /_
+    -  _your_project_name / src / static / css /_
+    -  _your_project_name / src / static / css /main.css_
+    -  _your_project_name / src / static / js /_
+    -  _your_project_name / src / static / js /main.js_
+    -  _your_project_name / src / static / media /_
 
-  - $ `python` `autodjango/autodjango.py` `your_project_name`
+5. _~/$_ `a_user_name` ... for the database and admin login
 
-3. Autodgango makes the following directories and files:
-  - _your_project_name / src / project_master / settings.py_
-  - _your_project_name / src / project_master /_
-  - _your_project_name / src / app_main /_
-  - _your_project_name / src / app_main / migrations /_
-  - _your_project_name / src / templates / base.html_
-  - _your_project_name / src / templates / app_main /_
-  - _your_project_name / src / templates / app_main / home.html_
-  - _your_project_name / src / static /_
-  - _your_project_name / src / static / css /_
-  - _your_project_name / src / static / css /main.css_
-  - _your_project_name / src / static / js /_
-  - _your_project_name / src / static / js /main.js_
-  - _your_project_name / src / static / media /_
+6. _~/$_ `an_email` ... or not ...
 
-4. $ `a_user_name`
+7. _~/$_ `a_password` ... twice
 
-5. $ `an_email` ... or not ...
+8. _~/$_ `y` ... to run the server now
 
-6. $ `a_password` ... twice
-
-7. $ `y` ... to run the server now
-
-8. Log into _localhost:8000 / admin_
-9. Go to _localhost:8000_ to see your empty home page.\
-Styles from _your_project_name / src / static / css /main.css_ are being applied; go replace them
-10. Open _~/ your_project_name /_ in a code editor and start <div>ing and .style{ ing }
+9. Log into **localhost:8000 / admin**
+10. Go to **localhost:8000** to see your empty home page.
+  - _~/../src/templates/app_main/home.html_\
+   is presented in a block, in the body of\
+  _~/../src/templates/base.html_;\
+ both of those files need `your_attention`.
+  - _~/../src/static/css/main.css_\
+  is styling those html files... nearly... `your_attention(main.css).also`.
+11. Open _~/.../_ in a code editor and make stuff happen avec the html, css, js, and py; allons-y!
--- autodjango_basic.py
+++ autodjango_basic.py
@@ -1,44 +1,54 @@
-#Creates a blank-ish, one-page django project called `your_project_name` in a virtual environment and runs the server.
-#- home page: ... _localhost:8000/_
-#- admin page: ... _localhost:8000/adimn/_
-#- database: ... _your_project_name /src / db.sqlite3_
-#- templates: ... _base.html_ ... _app_main / home.html_
-
-##### How to:
+## _autodjango_basic_
+##### What it do :
+#- creates a near-blank, one-page, **django web app** in `~/your_projects_directory/your_project_name`, hence forth writen as: _~/../_
+#- creates and activates a **virtual environment**
+#  - installs **django 3.0.8** and dependencies
+#  - starts a **django project** `project_master`
+#  - starts a **django app** `app_main`
+#  - creates and migrates **database :**\
+#    prompts for :\
+#    $ `user_name`\
+#    $ `user_password`\
+#    _~/../src/db.sqlite3_
+#  - creates and edits **html** templates :\
+#    _~/../src/templates/base.html_\
+#    _~/../src/templates/app_main/home.html_
+#  - and optionally, runs the server (y/n)\
+#    - home page : **localhost:8000**\
+#    - admin log in page : **localhost:8000/adimn**
+##### How to do :
 #1. Clone this repo into
 #_.~/your_projects_directory/_
-#. On the command line
-#  - $ `cd` `your_projects_directory`
+#2. _~/$_ `cd` `your_projects_directory`
+#3. _~/$_ `python` `autodjango/autodjango.py` `your_project_name`
+#4. Autodgango makes and edits the following directories and files:
+#    -  _your_project_name / src / project_master / settings.py_
+#    -  _your_project_name / src / project_master /_
+#    -  _your_project_name / src / app_main /_
+#    -  _your_project_name / src / app_main / migrations /_
+#    -  _your_project_name / src / templates / base.html_
+#    -  _your_project_name / src / templates / app_main /_
+#    -  _your_project_name / src / templates / app_main / home.html_
+#    -  _your_project_name / src / static /_
+#    -  _your_project_name / src / static / css /_
+#    -  _your_project_name / src / static / css /main.css_
+#    -  _your_project_name / src / static / js /_
+#    -  _your_project_name / src / static / js /main.js_
+#    -  _your_project_name / src / static / media /_
+#5. _~/$_ `a_user_name` ... for the database and admin login
+#6. _~/$_ `an_email` ... or not ...
+#7. _~/$_ `a_password` ... twice
+#8. _~/$_ `y` ... to run the server now
+#9. Log into **localhost:8000 / admin**
+#10. Go to **localhost:8000** to see your empty home page.
+#  - _~/../src/templates/app_main/home.html_\
+#   is presented in a block, in the body of\
+#  _~/../src/templates/base.html_;\
+# both of those files need `your_attention`.
+#  - _~/../src/static/css/main.css_\
+#  is styling those html files... nearly... `your_attention(main.css).also`.
+#11. Open _~/.../_ in a code editor and make stuff happen avec the html, css, js, and py; allons-y!
 
-# - $ `python` `autodjango/autodjango.py` `your_project_name`
-
-#3. Autodgango makes the following directories and files:
-#  - _your_project_name / src / project_master / settings.py_
-#  - _your_project_name / src / project_master /_
-#  - _your_project_name / src / app_main /_
-#  - _your_project_name / src / app_main / migrations /_
-#  - _your_project_name / src / templates / base.html_
-#  - _your_project_name / src / templates / app_main /_
-#  - _your_project_name / src / templates / app_main / home.html_
-#  - _your_project_name / src / static /_
-#  - _your_project_name / src / static / css /_
-#  - _your_project_name / src / static / css /main.css_
-#  - _your_project_name / src / static / js /_
-#  - _your_project_name / src / static / js /main.js_
-#  - _your_project_name / src / static / media /_
-
-#4. $ `a_user_name`
-
-#5. $ `an_email` ... or not ...
-
-#6. $ `a_password` ... twice
-
-#7. $ `y` ... to run the server now
-
-#8. Log into _localhost:8000 / admin_
-#9. Go to _localhost:8000_ to see your empty home page.\
-#Styles from _your_project_name / src / static / css /main.css_ are being applied; go replace them
-#10. Open _~/ your_project_name /_ in a code editor and start <div>ing and .style{ ing }
 
 import sys
 import os
@@ -49,14 +59,14 @@ os.system( f'clear' )
 # set constants according to platform
 SL = ''
 MAKE_FILE = ''
-PROJECT = str(sys.argv[1])
+PROJECT = str(sys.argv[1]) # project_name
 START_APP_MAIN = ''
 START_CHROME = ''
 CREATE_SUPER_USER = ''
 if(sys.platform == 'win32'):
     SL = '\\'
     MAKE_FILE = f'type nul >'
-    ACTIVATE_VENV = f'{ PROJECT }{ SL }scripts{ SL }activate_this.py'
+    ACTIVATE_VENV = f'{ PROJECT }{ SL }scripts{ SL }activate_this.py'# project_name\\sripts\\activate
     CREATE_SUPER_USER =  f'winpty python'
     START_CHROME = f'start chrome 127.0.0.1:8000 127.0.0.1:8000/admin'
 
